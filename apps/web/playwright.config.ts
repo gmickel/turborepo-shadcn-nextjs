6ffd5f7eb516c6d2deb78b7f8244b5a3944bad7b
Gordon Mickel <gordon.mickel@gmail.com>
2024-07-08 01:33:59 +0200

    chore(ci): improve e2e in ci action

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -1,25 +1,31 @@
 import path from 'node:path';
-import type { PlaywrightTestConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
-const config: PlaywrightTestConfig = {
-  webServer: {
-    command: 'bun run dev',
-    url: 'http://localhost:4000',
-    reuseExistingServer: !process.env.CI,
-    timeout: 120 * 1000,
-  },
+const config = defineConfig({
   testDir: path.join(__dirname, 'tests/e2e'),
   testMatch: '**/*.spec.ts',
   outputDir: path.join(__dirname, 'tests/e2e/coverage'),
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
   use: {
     baseURL: 'http://localhost:4000',
+    trace: 'on-first-retry',
   },
   projects: [
     {
       name: 'chromium',
-      use: { browserName: 'chromium' },
+      use: { ...devices['Desktop Chrome'] },
     },
   ],
-};
+  webServer: {
+    command: 'bun run dev:test',
+    url: 'http://localhost:4000',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
+  },
+});
 
 export default config;
